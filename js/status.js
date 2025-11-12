8082b0b76c4f70b55acbf371a0ddff398d08d9b5
투린 <Koomj5258@gmail.com>
2025-11-12 17:01:46 +0900

    Fix status API endpoint

--- js/status.js
+++ js/status.js
@@ -1,4 +1,4 @@
-const STATUS_API_URL = "https://mailbot.up.railway.app";
+const STATUS_API_URL = "https://mailbot.up.railway.app/api/state";
 const REQUEST_TIMEOUT = 8000;
 
 const indicator = () => document.getElementById("status-indicator");
